20f90a624d79bfbb76d22fc16d275b8c8b5694cb
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-05-15 02:52:44 +0530

    Time: 111 ms (24.86%), Space: 78.6 MB (50.63%) - LeetHub

---
+++ 2742-group-by/2742-group-by.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((result, item) => {
+        const key = fn(item);
+        if (!result[key]) {
+            result[key] = [];
+        }
+        result[key].push(item);
+        return result;
+    }, {});
+};
+
