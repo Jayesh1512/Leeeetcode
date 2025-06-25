f64577ce95ffd33c476a0ff99839dbcf87d50eee
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-06-26 00:40:36 +0530

    Time: 580 ms (96.49%), Space: 0B (100%) - LeetHub

---
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -0,0 +1,6 @@
+select s.user_id, ROUND(AVG(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END), 2) as confirmation_rate 
+from signups as s
+left join 
+confirmations as c
+on s.user_id=c.user_id
+group by s.user_id
