2bf1bf1951fe37ecc595380a99af25681cc890dd
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-06-27 12:04:43 +0530

    Time: 9 ms (48.23%), Space: 44.8 MB (78.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,30 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, ArrayList<Integer>> hm = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(nums[i])) {
-                ArrayList<Integer> ar = hm.get(nums[i]);
-                ar.add(i);
-                hm.put(nums[i], ar);
-            } else {
-                ArrayList<Integer> ar = new ArrayList<>();
-                ar.add(i);
-                hm.put(nums[i], ar);
-            }
+        int n = nums.length;
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; ++i) {
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (hm.containsKey(complement)) {
-                ArrayList<Integer> complementIndices = hm.get(complement);
-                for (int index : complementIndices) {
-                    if (index != i) {
-                        return new int[]{i, index};
-                    }
-                }
-            }
+        Arrays.sort(arr, Comparator.comparingInt(o -> o[0])); // Sort arr in increasing order by their values.
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum2 = arr[left][0] + arr[right][0];
+            if (sum2 == target)
+                return new int[]{arr[left][1], arr[right][1]};
+            if (sum2 > target)
+                right -= 1; // Try to decrease sum2
+            else
+                left += 1; // Try to increase sum2
         }
-        return new int[0];
+        return new int[]{};
     }
-    }
+}
