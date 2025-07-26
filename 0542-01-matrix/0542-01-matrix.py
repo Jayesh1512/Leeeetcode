de3ec9c2edbaf37449bb29efcbed45c3fc88f3b0
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-07-26 23:37:53 +0530

    Time: 111 ms (95.49%), Space: 16 MB (50.51%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,27 @@
+from collections import deque
+
 class Solution(object):
     def updateMatrix(self, mat):
         """
         :type mat: List[List[int]]
         :rtype: List[List[int]]
         """
+        from collections import deque
+
         rows = len(mat)
         cols = len(mat[0])
+        ans = [[-1 for _ in range(cols)] for _ in range(rows)]
 
-        ans = [[-1 for i in range(cols)] for i in range(rows)]  # -1 = unvisited
-
-        q = []
+        q = deque()
 
         for i in range(rows):
             for j in range(cols):
                 if mat[i][j] == 0:
                     q.append((i, j, 0))
-                    ans[i][j] = 0  # visited & distance 0
+                    ans[i][j] = 0
 
-        while len(q) != 0:
-            x, y, count = q.pop(0)
+        while q:
+            x, y, count = q.popleft()  # O(1)
 
             cDist = count + 1
 
