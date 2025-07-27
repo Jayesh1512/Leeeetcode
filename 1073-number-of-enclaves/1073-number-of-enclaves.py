51fa262a8d8de73eeb885a4af14f68e95f3876bb
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-07-27 14:00:52 +0530

    Time: 131 ms (54.78%), Space: 73.8 MB (47.83%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -6,18 +6,17 @@ class Solution(object):
         """
         ## Detect Border Cells -> DFS on Each 
         def dfs(x,y):
-            visited[x][y] == True
             grid[x][y] = 0
-            if x-1 >=0 and not visited[x-1][y] and grid[x-1][y] == 1:
+            if x-1 >=0  and grid[x-1][y] == 1:
                 dfs(x-1,y)
             
-            if y-1 >=0 and not visited[x][y-1] and grid[x][y-1] == 1:
+            if y-1 >=0  and grid[x][y-1] == 1:
                 dfs(x,y-1)
             
-            if x+1 < rows and not visited[x+1][y] and grid[x+1][y] == 1:
+            if x+1 < rows  and grid[x+1][y] == 1:
                 dfs(x+1,y)
             
-            if y+1 < cols and not visited[x][y+1] and grid[x][y+1] == 1:
+            if y+1 < cols  and grid[x][y+1] == 1:
                 dfs(x,y+1)
 
 
@@ -28,7 +27,6 @@ class Solution(object):
         cols = len(grid[0])
         ones = 0
 
-        visited = [[False for i in range(cols)]for i in range(rows)]
 
         for i in range(rows):
             for j in range(cols):
@@ -46,8 +44,7 @@ class Solution(object):
 
         for i in border:
             x,y = i
-            if not visited[x][y]:
-                dfs(x,y)
+            dfs(x,y)
 
         on = 0
         for i in range(rows):
