95ad45cc86bf531d0c4f140d5b89f9b4d0759964
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-08-01 00:04:40 +0530

    Time: 3 ms (28.14%), Space: 12.7 MB (11.54%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -21,5 +21,16 @@ class Solution(object):
             return up+ left
 
         dp = [[-1 for i in range(n)]for i in range(m)]
-        ans = rec(m-1,n-1)
-        return ans
+        
+        for i in range(m):
+            dp[i][0] = 1
+        
+        for i in range(n):
+            dp[0][i] = 1
+
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[-1][-1]
+
