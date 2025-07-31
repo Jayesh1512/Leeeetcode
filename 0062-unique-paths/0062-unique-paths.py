2b7f2e8c8dfe3e8cb361dce6501651e8a7a678d1
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-08-01 00:04:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,21 +5,7 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def rec(i,j):
-            if i == 0 and j == 0:
-                return 1
-            if i<0 or j<0 :
-                return 0
-
-            if dp[i][j] != -1:
-                return dp[i][j]
-
-            up = rec(i-1,j)
-            left = rec(i,j-1)
-
-            dp[i][j] = up+left
-            return up+ left
-
+    
         dp = [[-1 for i in range(n)]for i in range(m)]
         
         for i in range(m):
