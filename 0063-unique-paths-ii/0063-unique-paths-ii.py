79992731f33d8c455e6455defe4dec23a412899e
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-08-01 00:12:17 +0530

    Time: 0 ms (100%), Space: 12.6 MB (35.63%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,28 +1,23 @@
 class Solution(object):
-    def uniquePathsWithObstacles(self, obstacleGrid):
-        """
-        :type obstacleGrid: List[List[int]]
-        :rtype: int
-        """
-        def rec(i,j,grid,dp):
-            if grid[i][j] == 1:
-                return 0
-            if i == 0 and j == 0:
-                return 1
-            if dp[i][j] != -1:
-                return dp[i][j]
-            left =0
-            right=0
-            if i-1 >= 0:
-                left = rec(i-1,j,grid,dp)
-            if j-1 >= 0:
-                right = rec(i,j-1,grid,dp)
-            dp[i][j] = left+right
-            return left+right
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-        dp = [[-1 for _ in range(n)]for _ in range(m)]
-        # if obstacleGrid[0][0] == 1:
-        #     return 0
-        ans = rec(m-1,n-1,obstacleGrid,dp)   
-        return ans
+    def uniquePathsWithObstacles(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+
+        if grid[0][0] == 1:
+            return 0
+
+        dp[0][0] = 1
+
+        for i in range(1, m):
+            dp[i][0] = dp[i-1][0] if grid[i][0] == 0 else 0
+
+        for j in range(1, n):
+            dp[0][j] = dp[0][j-1] if grid[0][j] == 0 else 0
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if grid[i][j] == 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[m-1][n-1]
