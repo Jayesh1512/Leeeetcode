79662ab737693e78703027a50520eefc203a5b81
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-08-19 23:28:49 +0530

    Time: 631 ms (21.7%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,8 +1,15 @@
 # Write your MySQL query statement below
-SELECT 
-    distinct e.num ConsecutiveNums
-From
-    logs e CROSS JOIN logs l
-where l.id - e.id between 0 and 2 and e.num = l.num
-group by e.id
-having count(*) = 3
+select 
+    distinct l1.num as ConsecutiveNums
+from
+    logs l1
+inner join
+    logs l2
+inner join 
+    logs l3
+on 
+    l1.num = l2.num and
+    l2.num = l3.num
+where
+    l1.id = l2.id+1 and
+    l1.id = l3.id+2
