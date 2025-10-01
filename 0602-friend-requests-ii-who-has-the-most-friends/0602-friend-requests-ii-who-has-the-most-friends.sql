8226780569d93b07eb01f17c76e74b4ff6107fd4
Jayesh Shete <131896146+Jayesh1512@users.noreply.github.com>
2025-10-02 01:49:41 +0530

    Time: 354 ms (15.88%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,28 +1,13 @@
-WITH r AS (
-    SELECT requester_id, COUNT(*) AS cr
-    FROM RequestAccepted
-    GROUP BY requester_id
-), a AS (
-    SELECT accepter_id, COUNT(*) AS ar
-    FROM RequestAccepted
-    GROUP BY accepter_id
-), fmap as(
 SELECT 
-    COALESCE(r.requester_id, a.accepter_id) AS user_id,
-    ifnull(r.cr,0)+ifnull(a.ar,0) as counter
-FROM r
-LEFT JOIN a ON r.requester_id = a.accepter_id
-union
-SELECT 
-    COALESCE(r.requester_id, a.accepter_id) AS user_id,
-    ifnull(r.cr,0)+ifnull(a.ar,0) as counter
-FROM r
-RIGHT JOIN a ON r.requester_id = a.accepter_id
-)
-
-select user_id as id,counter as num
-from fmap
-where counter = (
-    select max(counter) from fmap
-)
-
+    id,
+    COUNT(*) AS num
+FROM (
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT requester_id AS id
+    FROM RequestAccepted
+) AS combined_ids
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
